d0a6e843e520a91d21082cfc26be033ea380148b
fireh1974 <64630396+fireh1974@users.noreply.github.com>
2023-02-04 23:54:33 +0800

    Update wdr4320.sh

--- wdr4320.sh
+++ wdr4320.sh
@@ -3,6 +3,8 @@ echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.de
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 
 ##### wdr4300-16M修改编译文件 ######
+sed -i 's/<0x020000 0x7d0000>/<0x020000 0xfd0000>/g' target/linux/ath79/dts/ar9344_tplink_tl-wdrxxxx.dtsi
+sed -i 's/<0x7f0000 0x010000>/<0xff0000 0x010000>/g' target/linux/ath79/dts/ar9344_tplink_tl-wdrxxxx.dtsi
 sed -i 's/tplink-8mlzma/tplink-16mlzma/g' target/linux/ath79/image/generic-tp-link.mk
 
 ####### Modify the version number
