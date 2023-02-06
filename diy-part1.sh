ee062189298730572bfcf2de804bc0493f5da667
fireh1974 <64630396+fireh1974@users.noreply.github.com>
2023-02-06 15:39:00 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,20 +1,3 @@
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
-
-####### Modify the version number
-sed -i "s/OpenWrt /fireh build from Lede $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
-
-echo '移除主页跑分信息显示'
-sed -i 's/ <%=luci.sys.exec("cat \/etc\/bench.log") or ""%>//g' package/lean/autocore/files/arm/index.htm
-echo '=========Remove benchmark display in index OK!========='
-
-# Modify default IP 
-sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
-
-# 移除不用软件包
-# rm -rf feeds/luci/applications/luci-app-netdata
-# rm -rf feeds/luci/applications/luci-app-pptp-server
-# rm -rf feeds/packages/net/smartdns
-# rm -rf feeds/packages/utils/syncthing
-# rm -rf feeds/packages/net/amule
