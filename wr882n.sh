9b4a2b1c9f17daec959eb6002c1d2742839c8414
fireh1974 <64630396+fireh1974@users.noreply.github.com>
2023-02-06 12:08:57 +0800

    Update wr882n.sh

--- wr882n.sh
+++ wr882n.sh
@@ -1,6 +1,6 @@
 ##### 16M修改编译文件 ######
 
-sed -i 's/tplink-2mlzma/tplink-16mlzma/g' target/linux/ar71xx/image/tiny-tp-link.mk
+sed -i 's/tplink-8mlzma/tplink-16mlzma/g' target/linux/ar71xx/image/tiny-tp-link.mk
 
 echo '修改主机名'
 sed -i "s/hostname='OpenWrt'/hostname='fireh-wr882n'/g" package/base-files/files/bin/config_generate
