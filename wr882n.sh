7833852f8aaa606e1b4acf9adf040de727c8ab73
fireh1974 <64630396+fireh1974@users.noreply.github.com>
2023-02-06 18:40:26 +0800

    Update wr882n.sh

--- wr882n.sh
+++ wr882n.sh
@@ -14,15 +14,15 @@ sed -i 's/luci-theme-bootstrap/luci-theme-argonne/g' feeds/luci/collections/luci
 sed -i "s/OpenWrt /fireh build from Lede $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
 
 echo '移除主页跑分信息显示'
-sed -i 's/ <%=luci.sys.exec("cat \/etc\/bench.log") or ""%>//g' package/lean/autocore/files/arm/index.htm
+sed -i 's/ <%=luci.sys.exec("cat \/etc\/bench.log") or ""%>//g' package/lean/autocore/files/index.htm
 echo '=========Remove benchmark display in index OK!========='
 
 # Modify default IP 
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
 # 移除不用软件包
- rm -rf feeds/luci/applications/luci-app-netdata
- rm -rf feeds/luci/applications/luci-app-pptp-server
- rm -rf feeds/packages/net/smartdns
- rm -rf feeds/packages/utils/syncthing
- rm -rf feeds/packages/net/amule
+# rm -rf feeds/luci/applications/luci-app-netdata
+# rm -rf feeds/luci/applications/luci-app-pptp-server
+# rm -rf feeds/packages/net/smartdns
+# rm -rf feeds/packages/utils/syncthing
+# rm -rf feeds/packages/net/amule
