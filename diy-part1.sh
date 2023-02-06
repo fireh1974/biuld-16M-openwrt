af7eb9c65307ca8078b56705440d8eb5e2c8f56c
fireh1974 <64630396+fireh1974@users.noreply.github.com>
2023-02-06 12:07:05 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,4 +1,4 @@
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+# echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 
@@ -16,8 +16,8 @@ sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generat
 sed -i 's/luci-theme-bootstrap/luci-theme-argonne/g' feeds/luci/collections/luci/Makefile
 
 # 移除不用软件包
-rm -rf feeds/luci/applications/luci-app-netdata
-rm -rf feeds/luci/applications/luci-app-pptp-server
-rm -rf feeds/packages/net/smartdns
-rm -rf feeds/packages/utils/syncthing
-rm -rf feeds/packages/net/amule
+# rm -rf feeds/luci/applications/luci-app-netdata
+# rm -rf feeds/luci/applications/luci-app-pptp-server
+# rm -rf feeds/packages/net/smartdns
+# rm -rf feeds/packages/utils/syncthing
+# rm -rf feeds/packages/net/amule
