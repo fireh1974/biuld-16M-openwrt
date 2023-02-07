3a60c9a6d6f77013a8ed4c9d73b4c1f3f47026cc
fireh1974 <64630396+fireh1974@users.noreply.github.com>
2023-02-07 09:10:59 +0800

    Update wr882n.sh

--- wr882n.sh
+++ wr882n.sh
@@ -19,9 +19,6 @@ sed -i 's/luci-theme-bootstrap/luci-theme-argonne/g' feeds/luci/collections/luci
 ####### Modify the version number
 sed -i "s/OpenWrt /fireh build from Lede $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
 
-echo '移除主页跑分信息显示'
-sed -i 's/ <%=luci.sys.exec("cat \/etc\/bench.log") or ""%>//g' package/lean/autocore/files/index.htm
-echo '=========Remove benchmark display in index OK!========='
 
 # Modify default IP 
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
