27bffe0496014f8bcd626949abf0cbb93e52905c
fireh1974 <64630396+fireh1974@users.noreply.github.com>
2023-02-09 15:56:43 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -6,9 +6,19 @@
 # See /LICENSE for more information.
 #
 # https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
-# echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+
+# Uncomment a feed source
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# Add a feed source
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+
+
+echo '添加alist'
+git clone https://github.com/sbwml/luci-app-alist package/alist
+echo '=========alist OK!========='
