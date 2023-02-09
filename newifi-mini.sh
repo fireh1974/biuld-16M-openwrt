eeaabbd4eff236b63e030642ad13fcf5df5f367c
fireh1974 <64630396+fireh1974@users.noreply.github.com>
2023-02-09 19:01:12 +0800

    Update newifi-mini.sh

--- newifi-mini.sh
+++ newifi-mini.sh
@@ -6,7 +6,7 @@
 # See /LICENSE for more information.
 #
 # https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
+# File name: newifi-mini.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
