ff30d4b172c9c9a15e3a8f4fb60baac9f4922218
fireh1974 <64630396+fireh1974@users.noreply.github.com>
2023-02-14 06:59:38 +0800

    Update wr841v5.sh

--- wr841v5.sh
+++ wr841v5.sh
@@ -11,7 +11,7 @@ sed -i "s/hostname='OpenWrt'/hostname=WR841n v5'/g" package/base-files/files/bin
 echo '=========Alert hostname OK!========='
 
 ##### Set argon as default theme
-sed -i 's/luci-theme-bootstrap/luci-theme-argonne/g' feeds/luci/collections/luci/Makefile
+#sed -i 's/luci-theme-bootstrap/luci-theme-argonne/g' feeds/luci/collections/luci/Makefile
 
 
 ####### Modify the version number
@@ -19,11 +19,11 @@ sed -i "s/OpenWrt /fireh build from Lede $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt 
 
 
 # Modify default IP 
-sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.3.1/g' package/base-files/files/bin/config_generate
 
 # 移除不用软件包
-# rm -rf feeds/luci/applications/luci-app-netdata
-# rm -rf feeds/luci/applications/luci-app-pptp-server
-# rm -rf feeds/packages/net/smartdns
-# rm -rf feeds/packages/utils/syncthing
-# rm -rf feeds/packages/net/amule
+ rm -rf feeds/luci/applications/luci-app-netdata
+ rm -rf feeds/luci/applications/luci-app-pptp-server
+ rm -rf feeds/packages/net/smartdns
+ rm -rf feeds/packages/utils/syncthing
+ rm -rf feeds/packages/net/amule
